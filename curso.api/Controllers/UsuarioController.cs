0ce3a45c011f2af974ab1133b84c5e418152eb61
vitor-ramalho <39497088+vitor-ramalho@users.noreply.github.com>
2021-09-20 23:57:50 -0300

    Adicionar arquivos de projeto.

---
+++ curso.api/Controllers/UsuarioController.cs
@@ -0,0 +1,54 @@
+﻿using curso.api.Filters;
+using curso.api.Models.Usuarios;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace curso.api.Controllers
+{
+    [Route("api/v1/usuario")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="loginViewModelInput"></param>
+        /// <returns>Retorna status ok, dados do usuario e o token em caso de sucesso</returns>
+        [SwaggerResponse(statusCode: 200, description: "Sucesso ao autenticar", Type = typeof(LoginViewModelInput))]
+        [SwaggerResponse(statusCode: 400, description: "Campos obrigatórios", Type = typeof(ValidaCampoViewModelOutput))]
+        [SwaggerResponse(statusCode: 500, description: "Erro Interno ", Type = typeof(ErroGenericoViewModel))]
+        [HttpPost]
+        [Route("logar")]
+        [ValidacaoModelStateCustomizado]
+        public IActionResult Logar(LoginViewModelInput loginViewModelInput)
+        {
+            //if (!ModelState.IsValid)
+            //{
+             //   return BadRequest(new ValidaCampoViewModelOutput(ModelState.SelectMany(sm => sm.Value.Errors).Select(s => s.ErrorMessage)));
+            //}
+            return Ok(loginViewModelInput);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="registroViewModelInput"></param>
+        /// <returns>Retorna status ok, dados do usuario e o token em caso de sucesso</returns>
+        [SwaggerResponse(statusCode: 200, description: "Sucesso ao autenticar", Type = typeof(LoginViewModelInput))]
+        [SwaggerResponse(statusCode: 400, description: "Campos obrigatórios", Type = typeof(ValidaCampoViewModelOutput))]
+        [SwaggerResponse(statusCode: 500, description: "Erro Interno ", Type = typeof(ErroGenericoViewModel))]
+        [HttpPost]
+        [Route("registrar")]
+        [ValidacaoModelStateCustomizado]
+        public IActionResult Registrar(RegistroViewModelInput registroViewModelInput)
+        {
+            return Created("", registroViewModelInput);
+        }
+    }
+}
